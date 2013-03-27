1b78394dbbfa9a664e11d14b1b0e5b3044ee262c
Hugh Saunders <hugh.saunders@rackspace.co.uk>
2013-03-27 17:23:26 +0000

    try external config file

--- opencenter-multi-node-gate.sh
+++ opencenter-multi-node-gate.sh
@@ -51,38 +51,10 @@ x_with_server "Running Happy Path Tests" ocserver <<EOF
 
 apt-get install -y git
 cd /opt
-git clone https://github.com/john5223/opencenter-testerator.git
+git clone https://github.com/hughsaunders/opencenter-testerator.git
 cd opencenter-testerator
-cat > gate-config <<EOCONFIG
- [test]
-\[test2\]
-[opencenter]
-endpoint_url = http://$(ip_for_host ocserver):8080
-instance_server_hostname = ocserver
-instance_chef_hostname = node1
-instance_controller_hostname = node2, node3
-instance_compute_hostname = node4, node5
-
-#user=
-#password=
-
-[cluster_data]
-osops_public = 10.0.0.0/8
-osops_mgmt = 10.0.0.0/8
-osops_nova = 10.0.0.0/8
-nova_public_if = eth0
-nova_vm_bridge = br100
-nova_dmz_cidr = 172.16.0.0/12
-cluster_name = test_cluster
-keystone_admin_pw = secrete
-nova_vm_fixed_if = eth1
-nova_vm_fixed_range = 192.168.200.0/24
-EOCONFIG
-echo "----- gate-config ------"
-cat gate-config
-echo "----- ------"
-export OPENCENTER_CONFIG_DIR="\$(pwd)"
-export OPENCENTER_CONFIG="gate-config"
+export OPENCENTER_CONFIG_DIR="\$(pwd)/etc"
+export OPENCENTER_CONFIG="opencenter-gate.conf"
 export OPENCENTER_ENDPOINT="http://$(ip_for_host ocserver):8080" 
 ./run_tests.sh -V
 EOF
